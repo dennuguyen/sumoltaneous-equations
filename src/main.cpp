6754092edb2eb8bed89ebbe08d5925479b2a1473
dennuguyen <dennuguyen@gmail.com>
2020-09-09 06:16:19 +1000

    Add sensor classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,21 @@
 #include <Arduino.h>
 
-/**
- * Pin has a pin number and value.
- */
 struct Pin {
+   public:
     Pin(int pno) : pno(pno), val(LOW) {}
+
+   public:
     int pno;  // pin number
     int val;  // pin value
 };
 
-/**
- * Wheels are driven by a DC motor which are encoded by two pins to give three
- * possible states: forward, backward, brake.
- */
-struct Wheel {
+class Wheel {
+   public:
     Wheel(int in1, int in2) : in1(Pin(in1)), in2(Pin(in2)) {}
+    void init(void) {
+        pinMode(in1.pno, OUTPUT);
+        pinMode(in2.pno, OUTPUT);
+    }
     void forward(void) {
         in1.val = HIGH;
         in2.val = LOW;
@@ -27,84 +28,140 @@ struct Wheel {
         in1.val = LOW;
         in2.val = LOW;
     }
+
+   public:
     Pin in1;
     Pin in2;
 };
 
-/**
- * Our bot only has four wheels.
- */
-struct Bot {
-    Bot(int lf1, int lf2, int rf1, int rf2, int lb1, int lb2, int rb1, int rb2)
-        : lf(Wheel(lf1, lf2)), rf(Wheel(rf1, rf2)), lb(Wheel(lb1, lb2)), rb(Wheel(rb1, rb2)) {}
-    void forward(void) {
-        lf.forward();
-        rf.forward();
-        lb.forward();
-        rb.forward();
+class Sonar {
+   public:
+    Sonar(int echo, int trig) : echo(echo), trig(trig) {}
+    void init(void) {
+        pinMode(echo.pno, INPUT);
+        pinMode(trig.pno, OUTPUT);
     }
-    void backward(void) {
-        lf.backward();
-        rf.backward();
-        lb.backward();
-        rb.backward();
+    void clear(void) {
+        digitalWrite(trig.pno, LOW);
     }
-    void brake(void) {
-        lf.brake();
-        rf.brake();
-        lb.brake();
-        rb.brake();
+    void send(void) {
+        digitalWrite(trig.pno, HIGH);
     }
-    void left(void) {
-        lf.backward();
-        rf.forward();
-        lb.backward();
-        rb.forward();
+    double receive(void) {
+        return pulseIn(echo.pno, HIGH) * 0.034 / 2;  // distance in cm
     }
-    void right(void) {
-        lf.forward();
-        rf.backward();
-        lb.forward();
-        rb.backward();
-    }
-    void write(void) {
-        digitalWrite(lf.in1.pno, lf.in1.val);
-        digitalWrite(lf.in2.pno, lf.in2.val);
-        digitalWrite(rf.in1.pno, rf.in1.val);
-        digitalWrite(rf.in2.pno, rf.in2.val);
-        digitalWrite(lb.in1.pno, lb.in1.val);
-        digitalWrite(lb.in2.pno, lb.in2.val);
-        digitalWrite(rb.in1.pno, rb.in1.val);
-        digitalWrite(rb.in2.pno, rb.in2.val);
-    }
-    Wheel lf;
-    Wheel rf;
-    Wheel lb;
-    Wheel rb;
+
+   public:
+    Pin echo;
+    Pin trig;
 };
 
-Bot b(PD0, PD1, PD2, PD3, PD4, PD5, PD6, PD7);  // Global object
+class Infrared {
+   private:
+    enum colours {
+        MIN = 0,
+        BLACK = 900,
+        MAX = 1023,
+    };
 
-void setup() {
-    pinMode(2, OUTPUT);  // ?
-    pinMode(PD0, OUTPUT);
-    pinMode(PD1, OUTPUT);
-    pinMode(PD2, OUTPUT);
-    pinMode(PD3, OUTPUT);
-    pinMode(PD4, OUTPUT);
-    pinMode(PD5, OUTPUT);
-    pinMode(PD6, OUTPUT);
-    pinMode(PD7, OUTPUT);
-}
+   public:
+    Infrared(int sig) : sig(sig) {}
+    void init(void) {
+        pinMode(sig.pno, INPUT);
+    }
+    bool edge(void) {
+        return analogRead(sig.pno) > BLACK;
+    }
 
-void loop() {
-    // Algorithm.
-    /**
-     * if (found) do something
-     * else search pattern
-     */
+   private:
+    Pin sig;
+};
 
-    // Write-out pins.
-    b.write();
-    delay(50);
+class Bot {
+   private:
+    enum wheels {
+        LF,
+        RF,
+        LB,
+        RB,
+    };
+    enum sensors {
+        FRONT,
+        LEFT,
+        RIGHT,
+        BACK,
+    };
+
+   public:
+    Bot(Wheel (&wheel)[4], Infrared (&gnd)[4], Sonar (&opp)[4])
+        : wheel(wheel), gnd(gnd), opp(opp) { init(); }
+    void init(void) {
+        for (int i = 0; i < 4; i++) {
+            wheel[i].init();
+            gnd[i].init();
+            opp[i].init();
+        }
+    }
+    void forward(void) {
+        wheel[LF].forward();
+        wheel[RF].forward();
+        wheel[LB].forward();
+        wheel[RB].forward();
+    }
+    void backward(void) {
+        wheel[LF].backward();
+        wheel[RF].backward();
+        wheel[LB].backward();
+        wheel[RB].backward();
+    }
+    void brake(void) {
+        wheel[LF].brake();
+        wheel[RF].brake();
+        wheel[LB].brake();
+        wheel[RB].brake();
+    }
+    void left(void) {
+        wheel[LF].backward();
+        wheel[RF].forward();
+        wheel[LB].backward();
+        wheel[RB].forward();
+    }
+    void right(void) {
+        wheel[LF].forward();
+        wheel[RF].backward();
+        wheel[LB].forward();
+        wheel[RB].backward();
+    }
+    void write(void) {
+        for (int i = 0; i < 4; i++) {
+            digitalWrite(wheel[i].in1.pno, wheel[i].in1.val);
+            digitalWrite(wheel[i].in2.pno, wheel[i].in2.val);
+        }
+    }
+
+   private:
+    Wheel (&wheel)[4];
+    Infrared (&gnd)[4];  // Infrared sensor detects boundary
+    Sonar (&opp)[4];     // Sonar detects opponent
+};
+
+int main(void) {
+    Wheel wheel[4] = {Wheel(PD0, PD1), Wheel(PD2, PD3), Wheel(PD4, PD5), Wheel(PD6, PD7)};
+    Infrared gnd[4] = {Infrared(A0), Infrared(A1), Infrared(A2), Infrared(A3)};
+    Sonar opp[4] = {Sonar(PD1, PD2), Sonar(PD1, PD2), Sonar(PD3, PD4), Sonar(PD5, PD6)};
+    Bot b(wheel, gnd, opp);
+
+    Serial.begin(9600);
+
+    while (1) {
+        // Check edges
+        for (int i = 0; i < 4; i++)
+            ;
+
+        // Write-out pins.
+        b.write();
+        delay(50);
+    }
+
+    return EXIT_SUCCESS;
 }
