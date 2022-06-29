01f23bf6ccd861bf09a082062d0cd52ba6cb4a77
dennuguyen <dennuguyen@gmail.com>
2022-06-30 05:55:16 +1000

    Fix bug.

--- sumoltaneous-equations-dev/DistanceSensor.hpp
+++ sumoltaneous-equations-dev/DistanceSensor.hpp
@@ -2,7 +2,7 @@
 
 class DistanceSensor {
    public:
-    DistanceSensor(unsigned trigger_pin, unsigned echo_pin, unsigned max_distance)
+    DistanceSensor(unsigned trigger_pin, unsigned echo_pin, unsigned max_distance = 0.004)
         : trigger_pin(trigger_pin), echo_pin(echo_pin), max_distance(max_distance) {
         pinMode(trigger_pin, OUTPUT);
         pinMode(echo_pin, INPUT);
@@ -11,7 +11,7 @@ class DistanceSensor {
     double echo() {
         digitalWrite(trigger_pin, false);
         delayMicroseconds(2);
-        delayWrite(trigger_pin, true);
+        digitalWrite(trigger_pin, true);
         delayMicroseconds(10);
         digitalWrite(trigger_pin, false);
         return pulseIn(echo_pin, HIGH) / 58.8235;
