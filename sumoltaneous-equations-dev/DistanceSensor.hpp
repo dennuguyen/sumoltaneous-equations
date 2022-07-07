c9b25a9fe842bd84c2ab9e6a4e07af2eeb9f3870
James Jiang <jiangjames1998@gmail.com>
2022-07-07 03:48:56 +0000

    Renamed variables.

--- sumoltaneous-equations-dev/DistanceSensor.hpp
+++ sumoltaneous-equations-dev/DistanceSensor.hpp
@@ -4,8 +4,8 @@
 
 class DistanceSensor {
    public:
-    DistanceSensor(unsigned trigger_pin, unsigned echo_pin, float timeout = 23529.4, float time_to_mm = 5.88235)
-        : trigger_pin(trigger_pin), echo_pin(echo_pin), timeout(timeout), time_to_mm(time_to_mm) {
+    DistanceSensor(unsigned trigger_pin, unsigned echo_pin, float timeout_ms = 23529.4, float ms_to_mm = 5.88235)
+        : trigger_pin(trigger_pin), echo_pin(echo_pin), timeout_ms(timeout_ms), ms_to_mm(ms_to_mm) {
         pinMode(trigger_pin, OUTPUT);
         pinMode(echo_pin, INPUT);
     }
@@ -18,14 +18,14 @@ class DistanceSensor {
         delayMicroseconds(10);
         digitalWrite(trigger_pin, LOW);
         noInterrupts();
-        float duration = pulseIn(echo_pin, HIGH, timeout);
+        float duration_ms = pulseIn(echo_pin, HIGH, timeout_ms);
         interrupts();
-        return duration / time_to_mm;
+        return duration_ms / ms_to_mm;
     }
 
    private:
     unsigned trigger_pin;
     unsigned echo_pin;
-    float timeout;
-    float time_to_mm;
+    float timeout_ms;
+    float ms_to_mm;
 };
