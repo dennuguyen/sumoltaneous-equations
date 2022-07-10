b9ae889fc3c6fe858385cd8d4071a4732b057818
dennuguyen <dennuguyen@gmail.com>
2022-07-11 02:04:37 +1000

    Remove read() as it is not needed due to interrupt.

--- sumoltaneous-equations-dev/IRSensor.hpp
+++ sumoltaneous-equations-dev/IRSensor.hpp
@@ -4,20 +4,11 @@
 
 class IRSensor {
    public:
-    IRSensor(unsigned read_pin, void* callback = read) : read_pin(read_pin) {
+    IRSensor(unsigned read_pin, void* callback) : read_pin(read_pin) {
         pinMode(read_pin, INPUT);
         attachInterrupt(digitalPinToInterrupt(read_pin), callback, HIGH);
     }
 
-    void read() {
-        detected = digitalRead(read_pin);
-    }
-
-    bool read() {
-        return detected;
-    }
-
    private:
     unsigned read_pin;
-    bool detected;
 };
