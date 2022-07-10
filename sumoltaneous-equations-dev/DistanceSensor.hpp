897d96587af84baa8671f8b04a5a8f9a67d911b8
dennuguyen <dennuguyen@gmail.com>
2022-07-11 02:06:09 +1000

    Clean style. Use ternary to handle no detection case.

--- sumoltaneous-equations-dev/DistanceSensor.hpp
+++ sumoltaneous-equations-dev/DistanceSensor.hpp
@@ -4,18 +4,18 @@
 
 class DistanceSensor {
    public:
-    DistanceSensor(unsigned trigger_pin, unsigned echo_pin, float timeout_ms = 23529.4, float ms_to_mm = 5.88235)
+    DistanceSensor(unsigned trigger_pin,
+                   unsigned echo_pin,
+                   float timeout_ms = 23529.4,
+                   float ms_to_mm = 5.88235)
         : trigger_pin(trigger_pin), echo_pin(echo_pin), timeout_ms(timeout_ms), ms_to_mm(ms_to_mm) {
         pinMode(trigger_pin, OUTPUT);
         pinMode(echo_pin, INPUT);
     }
 
-    // We have to call echo continuously.
-    // The echo_pin can be made interruptable which interrupts the flow of the code.
-    // The callback function on interrupt is the sending of a move command.
-
-    // returns distance in mm as a float
-    float echo() {
+    // Function triggers a pulse for some number of milliseconds then returns the distance to the
+    // detected obstacle or 0 otherwise.
+    auto echo() const -> float {
         digitalWrite(trigger_pin, LOW);
         delayMicroseconds(2);
         digitalWrite(trigger_pin, HIGH);
@@ -24,7 +24,7 @@ class DistanceSensor {
         noInterrupts();
         float duration_ms = pulseIn(echo_pin, HIGH, timeout_ms);
         interrupts();
-        return duration_ms / ms_to_mm;
+        return duration_ms != 0 ? duration_ms / ms_to_mm : timeout_ms / ms_to_mm;
     }
 
    private:
