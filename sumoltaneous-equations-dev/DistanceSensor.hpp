379ee23582233a374c5efeddd8443280d9bbcf5a
dennuguyen <dennuguyen@gmail.com>
2022-07-14 14:56:25 +1000

    Fix bug.

--- sumoltaneous-equations-dev/DistanceSensor.hpp
+++ sumoltaneous-equations-dev/DistanceSensor.hpp
@@ -4,7 +4,7 @@
 
 class DistanceSensor {
    public:
-    DistanceSensor(unsigned trigger_pin, unsigned echo_pin, float timeout_ms = 23529.4, float pulse_on_duration_ms = 10, float pulse_off_duration_ms = 2)
+    DistanceSensor(unsigned trigger_pin, unsigned echo_pin, float timeout_ms = 23529.4, float ms_to_mm = 5.88235, float pulse_on_duration_ms = 10, float pulse_off_duration_ms = 2)
         : trigger_pin(trigger_pin), echo_pin(echo_pin), timeout_ms(timeout_ms), ms_to_mm(ms_to_mm), pulse_on_duration_ms(pulse_on_duration_ms), pulse_off_duration_ms(pulse_off_duration_ms) {
         pinMode(trigger_pin, OUTPUT);
         pinMode(echo_pin, INPUT);
@@ -25,11 +25,10 @@ class DistanceSensor {
     }
 
    private:
-    const float ms_to_mm = 5.88235;
     const unsigned trigger_pin;
     const unsigned echo_pin;
-    const float timeout_ms;
     const float ms_to_mm;
+    const float timeout_ms;
     const float pulse_on_duration_ms;
     const float pulse_off_duration_ms;
 };
